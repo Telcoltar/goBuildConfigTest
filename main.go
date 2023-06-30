b5d718948c0416899b086a27c50567182a625a18
Nils Schulenberg <nils.schulenberg@me.com>
2023-06-30 14:52:30 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type greeting struct {
+	ID      string `json:"id"`
+	Content string `json:"title"`
+}
+
+var greetings = []greeting{
+	{ID: "1", Content: "Hello"},
+	{ID: "2", Content: "World!"},
+}
+
+func getGreetings(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, greetings)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getGreetings)
+
+	router.Run("localhost:8080")
+}
